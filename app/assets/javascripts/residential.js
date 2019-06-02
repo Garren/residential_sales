a91f48a82350712d1243e4cfc54ccdae00c989a5
Adam Garren <garren@users.noreply.github.com>
2019-06-01 19:19:25 -0600

    add a bar chart
    add a bar chart describing median home values by zipcode for a single
    jurisdiction.

--- app/assets/javascripts/residential.js
+++ app/assets/javascripts/residential.js
@@ -1,3 +1,62 @@
+function makeBar() {
+  var margin = { top: 20, right: 20, bottom: 50, left: 50 }
+    , width  = 960 - margin.left - margin.right
+    , height = 500 - margin.top - margin.bottom;
+
+  var xValue = function(d) { return d.zipcode; }
+    , xScale = d3.scale.ordinal().rangeRoundBands([0, width], .1)
+    , xMap   = function(d) { return xScale(xValue(d)); }
+    , xAxis  = d3.svg.axis().scale(xScale).orient("bottom");
+
+  var yValue = function(d) { return d.median_value; }
+    , yScale = d3.scale.linear().range([height, 0])
+    , yMap   = function(d) { return yScale(yValue(d)); }
+    , yAxis  = d3.svg.axis().scale(yScale).orient("left");
+
+  var svg = d3.select("#chart").append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+              .attr("transform", "translate("+ margin.left +","+ margin.top +")");
+
+  $.getJSON("/residential/bar_data", function(data) {
+    data = data.bar_data;
+    xScale.domain(data.map(xValue));
+    yScale.domain([0, d3.max(data, yValue)]);
+
+    svg.append("g")
+       .attr("class", "x axis")
+       .attr("transform", "translate(0,"+ height +")")
+       .call(xAxis)
+       .selectAll("text")
+       .attr("x", 8)
+       .attr("y", -5)
+       .style("text-anchor", "start")
+       .attr("transform", "rotate(90)");
+
+    svg.append("g")
+       .attr("class", "y axis")
+       .call(yAxis)
+       .append("text")
+       .attr("transform", "rotate(-90)")
+       .attr("y", 6)
+       .attr("dy", ".71em")
+       .style("text-anchor", "end")
+       .text("Median Value");
+    
+    svg.selectAll(".bar")
+       .data(data)
+       .enter()
+       .append("rect")
+       .attr("class", "bar")
+       .style("fill", "blue")
+       .attr("x", xMap)
+       .attr("width", xScale.rangeBand)
+       .attr("y", yMap)
+       .attr("height", function(d) { return height - yMap(d); });
+  });
+}
+
 function makePie() {
   var width  = 600,
       height = width,
@@ -23,7 +82,7 @@ function makePie() {
               .attr("transform", "translate("+ width/2  +","+ height/2 +")") 
 
   // fetch our data and draw the slices
-  $.getJSON('residential/data', function(data) {
+  $.getJSON('/residential/data', function(data) {
     totals = data.totals;
     // generate svg elements for the data
     var g = svg.selectAll(".arc")
--- app/controllers/residential_controller.rb
+++ app/controllers/residential_controller.rb
@@ -1,10 +1,18 @@
 class ResidentialController < ApplicationController
-  def index
-  end
+  def index; end
 
   def data
     # get the sum of total sales by county
     totals = SalesFigure.group(:jurisdiction).sum(:total_sales)
     render :json => { :totals => totals }
+  end
+
+  def bar_chart; end
+
+  def bar_data
+    bar_data = SalesFigure.select(:id, :zipcode, :median_value)
+                          .where(:jurisdiction => 'Baltimore')
+                          .order('median_value DESC, zipcode')
+    render :json => { :bar_data => bar_data }
   end
 end
